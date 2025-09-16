0b1519950b9c4517521fb998302b932affcb4133
gabriel <gc25719@email.vccs.edu>
2025-09-15 20:27:54 -0400

    Sea level rise 1.5 x x7 x10. displayed for each year

--- Chapter2Exercise7OceanLevels.cpp
+++ Chapter2Exercise7OceanLevels.cpp
@@ -1,9 +1,28 @@
-// Chapter2Exercise7OceanLevels.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+ Project Name : Chapter 2 Exercise 7 Ocean Levels
+ File Name : Chapter2Exercise7OceanLevels.cpp
+ Programmer : Gabriel Cabral
+ Date : 09/15/25
+ Requirements:
+ Rising ocean level by 1.5 milimeters by 5,7,10 years
+*/
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //set all nuumbers in mm
+    double lengthInMillimeters = 1.5;
+    std::cout << "The Oceans Levels rise " << lengthInMillimeters << "mm" << " every year." << std::endl;
+    //ocean grows 1.5mm per year
+
+    //ocean level in 5 yr
+    std::cout << "The ocean will rise " << lengthInMillimeters * 5 << "mm" << " in 5 years. " << std::endl;
+
+    //ocean level in 7 yr
+    std::cout << "The ocean will rise " << lengthInMillimeters * 7 << "mm" << " in 7 years. " << std::endl;
+
+    //ocean level in 10 yr
+    std::cout << "The ocean will rise " << lengthInMillimeters * 10 << "mm" << " in 10 years. " << std::endl;
+
+    return 0;
 }
